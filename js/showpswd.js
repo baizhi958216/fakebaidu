54e8b252688d72902a330fbad5cf4e05de13d9ef
baizhi958216 <1475289190@qq.com>
2021-06-15 19:06:30 +0800

    验证账号邮箱密码是否存在/加入返回主页svg

--- js/showpswd.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $("#showpswd").on("mouseover", function() {
-        $("#pass").prop("type", "text");
-    });
-    $("#showpswd").on("mouseout", function() {
-        $("#pass").prop("type", "password");
-    });
-});
