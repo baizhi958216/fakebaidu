7a51ba788d493bee4c19392383b9b2739a4b5d80
baizhi958216 <1475289190@qq.com>
2022-05-19 23:02:33 +0800

    update devicelist
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- javascript/finddevices.js
+++ javascript/finddevices.js
@@ -10,18 +10,10 @@ $('.tab a').on('click', function () {
     fetch('/assets/overview/official.json')
     .then(res=>res.json())
     .then(json=>{
-        $('.dlist')[0].innerHTML=''
-        for (const key in json) {
-            if(json[key].manufacturer==deviceIs){
-                for (const infos in json[key].info) {
-                    $('.dlist')[0].innerHTML+=`
-                    <div><!==${json[key].info[infos].deviceName}--></div>
-                    `
-                }
-            }
-        }
+        createCards(json)
     })
 })
+
 // 监听窗口resize,scroll,load操作，动态配置页面大小 记录浏览位置
 $(window).on('resize', () => {
     getWindowSize()
@@ -32,4 +24,26 @@ function getWindowSize() {
         'width': $(window).width() + 'px',
         'height': $(window).height()-108 + 'px'
     })
+}
+
+function createCards(json){
+    $('.dlist')[0].innerHTML=''
+        for (const key in json) {
+            if(json[key].manufacturer==deviceIs){
+                for (const infos in json[key].info) {
+                    $('.dlist')[0].innerHTML+=`
+                    <div id="device_card">
+                        <div id="devicepic" style="background-image:url(/assets/devicepic/${json[key].info[infos].codeName}.jpg);"></div>
+                        <div>${json[key].info[infos].deviceName}</div>
+                        <div>made by ${json[key].manufacturer}</div>
+                        <div>codename: <span>${json[key].info[infos].codeName}</span></div>
+                        <div id="dlLatest" dvs="${json[key].info[infos].codeName}"">DOWNLOAD LATEST VERSION</div>
+                    </div>
+                    ` 
+                }
+            } 
+        }
+        $('#dlLatest').on('click',function(){
+            console.log($(this)[0].getAttribute('dvs'));
+        }) 
 }
